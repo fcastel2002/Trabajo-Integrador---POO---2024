17ca7ddb7cda43b973c5588259f8290bc2147ef7
fcastel2002 <146264604+fcastel2002@users.noreply.github.com>
2024-10-09 16:00:53 -0300

    ...

--- cliente_files/MainMenu.cpp
+++ cliente_files/MainMenu.cpp
@@ -6,11 +6,31 @@
 
 #include <iostream>
 #include <ostream>
-MainMenu *mainMenu;
-void mostrarMenu() {
+
+void MainMenu::mostrarMenu() {
     system("cls");
     std::cout << "=======MENU======" << std::endl;
-    for(const auto& opcion : mainMenu->opciones) {
+    for(const auto& opcion : opciones) {
+        std::cout << opcion << std::endl;
+    }
+    std::cout << "Seleccione una opcion" << std::endl;
+}
+void MainMenu::seleccionarOpcion(int opcion) {
+    switch(opcion) {
+        case 1:
+            cliente ->conectar();
+            break;
+        case 2:
+            std::string nombreUsuario,clave;
+        std::cout << "Ingrese el nombre del usuario: ";
+        std::cin >> nombreUsuario;
+        std::cout << "Ingrese clave: ";
+        std::cin >> clave;
+        cliente -> autenticar(nombreUsuario, clave);
+        break;
+        case 3:
+            std::string tipo,parametros;
+            
 
     }
 }
