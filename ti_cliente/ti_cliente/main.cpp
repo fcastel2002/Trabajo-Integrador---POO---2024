cac9c81f51bf77c6150902d3df853b12d773c446
fcastel2002 <146264604+fcastel2002@users.noreply.github.com>
2024-10-16 12:47:27 -0300

    .

--- ti_cliente/ti_cliente/main.cpp
+++ ti_cliente/ti_cliente/main.cpp
@@ -1,37 +1,35 @@
-// src/cliente.cpp
 #include <iostream>
-#include <XmlRpc.h> // Asegúrate de que la ruta de inclusión está configurada correctamente
 #include "Cliente.h"
 #include "CLIMessageView.h"
 #include "MainMenu.h"
 
 int main() {
-	CLIMessageView messages;
+    CLIMessageView messages;
 
-	Cliente cliente("127.0.0.1", 9000, messages);
+    Cliente cliente("127.0.0.1", 9000, messages);
 
-	MainMenu menu(messages);
+    MainMenu menu(messages);
 
-	menu.addOption(1, "Activar motores", "activar_motores");
-	menu.addOption(2, "Desactivar motores", "desactivar_motores");
-	menu.addOption(3, "Homming", "homming");
+    menu.addOption(1, "Activar motores", "activar_motores");
+    menu.addOption(2, "Desactivar motores", "desactivar_motores");
+    menu.addOption(3, "Homming", "homming");
 
-	while (true) {
-		std::optional<std::string> cmdOption = menu.displayMenu();
-		if (!cmdOption.has_value()) {
-			messages.showMessage("Saliendo del programa", MessageType::INFO);
-			break;
-		}
+    while (true) {
+        std::optional<std::string> cmdOption = menu.displayMenu();
+        if (!cmdOption.has_value()) {
+            messages.showMessage("Saliendo del programa", MessageType::INFO);
+            break;
+        }
 
-		std::string comando = cmdOption.value();
-		messages.showMessage("Enviando comando: " + comando, MessageType::INFO);
-		bool success = cliente.enviarComando(comando);
-		if (success) {
-			messages.showMessage("Comando ejecutado correctamente", MessageType::INFO);
-		}
-		else {
-			messages.showMessage("Error al ejecutar el comando", MessageType::ERROR);
-		}
-	}
-	return 0;
+        std::string comando = cmdOption.value();
+        messages.showMessage("Enviando comando: " + comando, MessageType::INFO);
+        bool success = cliente.enviarComando(comando);
+        if (success) {
+            messages.showMessage("Comando ejecutado correctamente", MessageType::INFO);
+        }
+        else {
+            messages.showMessage("Error al ejecutar el comando", MessageType::ERROR);
+        }
+    }
+    return 0;
 }
