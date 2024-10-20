a61b7295c394cabeaddbcda9ebbc508ad1361a56
fcastel2002 <146264604+fcastel2002@users.noreply.github.com>
2024-10-20 00:24:29 -0300

    ...

---
+++ ti_cliente_v2/ti_cliente/Orden.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+class Orden {
+private:
+	std::string m_tipo{};
+	std::vector<std::string> m_parametros{};
+public:
+
+	Orden(const std::string& tipo, const std::vector<std::string>& parametros) : m_tipo(tipo), m_parametros(parametros) {}
+
+	std::string toJson(std::string& username, std::string& pass) const {
+		nlohmann::json j;
+		j["username"] = username;
+		j["password"] = pass;
+		j["tipo"] = m_tipo;
+		j["parametros"] = m_parametros;
+		j["cantidad_param"] = m_parametros.size();
+		return j.dump();
+	}	
+
+};
