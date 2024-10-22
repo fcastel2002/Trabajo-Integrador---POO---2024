0c712671940d756b159f47423dc1d933c7da960b
fcastel2002 <146264604+fcastel2002@users.noreply.github.com>
2024-10-22 15:34:06 -0300

    borre ti_cliente, deje ti_cliente_v2

--- ti_cliente/ti_cliente/CLIMessageView.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IMessageView.h"
-class CLIMessageView :
-    public IMessageView 
-{
-public:
-	void showMessage(const std::string& message, MessageType type = MessageType::INFO) override;
-};
-
