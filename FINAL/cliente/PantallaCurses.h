16484f6d5cae06ad5c93252685a7ca2d587a34a0
MatiasArmani <armanimatias@gmail.com>
2024-10-28 19:01:11 -0300

    final

---
+++ FINAL/cliente/PantallaCurses.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "IPantalla.h"
+#include "Archivo.h"
+class PantallaCurses :
+    public IPantalla
+{
+public:
+    PantallaCurses();
+	~PantallaCurses() override;
+
+	int mostrarMenu(const std::vector<std::string>& opciones, const std::string& tituloMenu) override;
+	void mostrarTexto(const std::string& mensaje) override;
+	void limpiarPantalla() override;
+	std::string capturarEntrada(const std::string& mensaje) override;
+	std::vector<std::string> capturarEntradaMultiple(std::string& mensaje) override;
+	std::string capturarEleccion(const std::string& mensaje, const std::vector<std::string>& opciones) override;
+	void refrescarPantalla() override;
+	void  mostrarError(const std::string& error) override;
+
+};
+
---
+++ FINAL/servidor/ManejadorErrores.py
@@ -0,0 +1,53 @@
+class ErrorDeConexion(Exception):
+    def __init__(self, tipo):
+        mensajes = {
+            1: "Falla: No hay conexion con el sistema. Verifique el comando 'conectar'.",
+            2: "Falla: El sistema esta inactivo. Verifique que los motores esten encendidos."
+        }
+        self.mensaje = mensajes.get(tipo, "Error de conexion desconocido.")
+
+    def __str__(self):
+        return self.mensaje
+
+
+class ErrorDeParametros(Exception):
+    def __init__(self, tipo):
+        mensajes = {
+            1: "Parametros fuera de los limites aceptados.",
+            2: "Numero de parametros incorrecto. Use 'ayuda <comando>' para mas informacion.",
+            3: "Parametros invalidos. Use 'ayuda <comando>' para mas informacion."
+        }
+        self.mensaje = mensajes.get(tipo, "Error de parametros.")
+
+    def __str__(self):
+        return self.mensaje
+
+
+class ErrorDeEstado(Exception):
+    def __init__(self, tipo):
+        mensajes = {
+            1: "Los motores ya estan apagados.",
+            2: "Los motores ya estan encendidos.",
+            3: "La conexión ya esta activa.",
+            4: "La conexión ya esta inactiva.",
+            5: "El efector ya esta activado.",
+            6: "El efector ya esta desactivado.",
+            7: "El robot ya esta en modo aprendizaje.",
+            8: "El robot no esta en modo aprendizaje.",
+            9: "No se puede ejecutar en modo aprendizaje activo."
+        }
+        self.mensaje = mensajes.get(tipo, "Error de estado.")
+
+    def __str__(self):
+        return self.mensaje
+
+class ErrorArchivos(Exception):
+    def __init__(self, tipo, nombre_archivo = None):
+        mensajes = {
+            1: "No se pudo escribir en el archivo",
+            2: f"El archivo {nombre_archivo}.txt no existe."
+        }
+        self.mensaje = mensajes.get(tipo, "Error de estado.")
+
+    def __str__(self):
+        return self.mensaje
